c3bbaadb8ec7e43038157d423ad99b976cfacc2e
15rudtp <thdrudtp15@naver.com>
2024-04-16 17:28:28 +0900

    useCallback 최적화 정리

--- rtl-tdd-todos/src/TodoApp.js
+++ rtl-tdd-todos/src/TodoApp.js
@@ -31,7 +31,22 @@ const TodoApp = () => {
     },[])
 
     const onToggle = useCallback(id => {
-        setTodos(todos => todos.map(item => item.id === id ? {...item,done : !item.done} : item))
+        /*
+            useCallback에서 의존성 배열 비우고 이렇게 쓰면 무조건 버그 발생
+            setTodos(todos.map(item => item.id === id ? {...item,done : !item.done} : item))
+        */
+            setTodos(todos => todos.map(item => item.id === id ? {...item, done : !item.done} : item));
+        // useCallback은 함수 자체를 메모이제이션한다고 생각하면 됨.
+        // setState(!prev) 이런식으로 이전 값을 감지하지 않고 setter함수를 사용한다는 것은 
+        // 그냥 초기값만을 가지고 값을 변경한다고 생각하면 됨.
+        // 즉 초기 초기 값도 메모이제이션 된다고 생각하면 됨.
+        // 이렇게 사용하기 위해서는 의존성 배열에 변경 값을 넣어 주어야 하는데(prev의 값을 재조정 해야 하니까)
+        // 그러면 또 React.memo에 함수가 들어있는 경우 최적화에 아무 도움이 안 됨. (todos를 사용한다는 가정하에)
+        // 함수를 최적화하려면(함수의 메모리 위치 변경으로 인한 리랜더링을 방지하려면)
+        // 의존성 배열을 빈 배열로 유지하고 setState(prev => !prev)이런 식으로 이전 값을 
+        // 참조하는 방법으로 사용 해야 함.
+        // 그렇게 하면 변경된 값을 감지 가능할 뿐더러 함수의 메모리 재할당을 막아 의도한대로 
+        // 최적화가 가능함.
     },[])
 
     const onRemove = useCallback(id => {
--- rtl-tdd-todos/src/TodoItem.js
+++ rtl-tdd-todos/src/TodoItem.js
@@ -2,6 +2,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 
 const TodoItem = React.memo(({todo,onToggle,onRemove}) => {
+    // 사실 useCallback을 안 해주면 onToggle이랑 onRemove때문에 최적화 하나도 안되고 
+    // 오히려 리소스 낭비함 (React.memo는 추가적인 비교 연산이 들어감)
+    // 그래서 onToggle같은 함수를 받아서 사용하는데 todo가 변경되었을 때만 리랜더링 하고 싶으면
+    // 부모 컴포넌트에서 useCallback으로 최적화 해주어야 함.
     const {id ,text ,done} = todo;
     const toggle = useCallback(()=> onToggle(id) ,[id,onToggle]);
     const remove = useCallback(() => onRemove(id), [id,onRemove]);
