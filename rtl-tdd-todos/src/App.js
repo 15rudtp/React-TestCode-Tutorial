dd55b0ba0f6e31113ff120ebb4d94d660ab067bd
15rudtp <thdrudtp15@naver.com>
2024-04-19 18:51:14 +0900

    비동기 테스트 끝 API요청 테스트 시작

--- rtl-tdd-todos/src/App.js
+++ rtl-tdd-todos/src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import TodoApp from './TodoApp';
 import DelayedToggle from "./DelayedToggle.js";
+import UserProfile from './UserProfile.js';
 
 function App() {
   return (
     // <TodoApp />
-    <DelayedToggle />
+    //<DelayedToggle />
+    <UserProfile id={1}/>
     );
 }
 
--- rtl-tdd-todos/src/DelayedToggle.test.js
+++ rtl-tdd-todos/src/DelayedToggle.test.js
@@ -4,11 +4,18 @@ import {
     render,
     fireEvent,
     // waitFor,
-    waitForElement,
-    // waitForDomChange,
-    // waitForElementToBeRemoved
+    // waitForElement,
+    waitForElementToBeRemoved,
     waitFor
 } from '@testing-library/react';
+
+/**
+ * @waitFor :wait, waitForElement 등은 테스팅 라이브러리가 업데이트 되면서 waitFor로 통합됨. 기본시간 1000ms
+ * @getByText : 텍스트로 요소를 찾음, 요소 없으면 오류 발생
+ * @queryByText : 텍스트로 요소를 찾음, 요소 없어도 null 반환
+ * @waitForElementToBeRemoved : 특정 요소가 사라질 때까지 기다림. 기본시간 1000ms
+ */
+
 
 describe('<DelayedToggle/>', () => {
     it('reveals text when toggle is ON' , async () => {
@@ -25,5 +32,23 @@ describe('<DelayedToggle/>', () => {
         fireEvent.click(toggleButton);
         const text = await waitFor(()=>getByText("ON"),{timeout : 1600});
         expect(text).toHaveTextContent("ON");
+    })
+
+    it('changes something when button is clicked', async () => {
+        const { getByText, queryByText } = render(<DelayedToggle/>);
+        const toggleButton = getByText('토글');
+        fireEvent.click(toggleButton);
+        const mutation = await waitFor(()=> expect(getByText("토글")).toHaveTextContent("토글"));
+        console.log(mutation);
+    })
+
+    it('removes text when toggle is OFF', async () => {
+        const {getByText} = render(<DelayedToggle/>);
+        const toggleButton = getByText("토글");
+        fireEvent.click(toggleButton);
+        await waitForElementToBeRemoved(()=>getByText("OFF"),{timeout : 1500});
+        fireEvent.click(toggleButton);
+        await waitForElementToBeRemoved(()=>getByText("야호!!"), {timeout : 1500})
+
     })
 })
